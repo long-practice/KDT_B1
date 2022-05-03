3db95f6a106cb06883a32041fce1c6c549d8498f
HRDI0 <gjtjdals2546@gmail.com>
2022-05-03 18:10:15 +0900

    fix batch_voice.py

--- Tacotron2-Wavenet-Korean-TTS/batch_voice.py
+++ Tacotron2-Wavenet-Korean-TTS/batch_voice.py
@@ -15,34 +15,62 @@ temp_dir = '/home/workspace/tmp_dir'
 #save_dir = '/home/workspace/target/'
 save_dir = '/home/workspace/app/KDT_B1_2/b1project/homepage/static/file_audio/' + opt.id
 
-try:
-    f = open('/home/workspace/folktale_full_data', 'r')
-except:
-    print('no such data or dir')    
-
-# row[2] : contents / row[3] : talker_identifier / row[4] : title_id / row[1] : line_index
-
 sysnthesizer_cmd = '/home/workspace/model/KDT_B1/Tacotron2-Wavenet-Korean-TTS/synthesizer.py'
 load_path = '--load_path logdir-tacotron2/++++++++++_2022-04-25_05-21-56'
 num_speaker = '--num_speakers 11'
 sample_path = '--sample_path ' + temp_dir
 fixed_cmd ='python' + ' ' + sysnthesizer_cmd + ' ' +load_path + ' '  + sample_path + ' '+ num_speaker
 
-if not os.path.isdir(save_dir[:-2]):
-    os.mkdir(save_dir[:-2])
+if not os.path.isdir(save_dir):
+    os.mkdir(save_dir)
 
-if not os.path.isdir(sample_path.split()[1]):
-    os.mkdir(sample_path.split()[1])
+if not os.path.isdir(temp_dir):
+    os.mkdir(temp_dir)
 
-cnt = 0
-lines = f.readlines()
-for line in lines :
-    #if cnt >= 3:
-        #break
-    for i in range(11):
-        print(fixed_cmd +' ' + '--speaker_id' + ' ' + str(i) + ' ' + '--text' + ' "' + line.split('\t')[0]  + '."')
-        os.system(fixed_cmd +' ' + '--speaker_id' +' ' + str(i) + ' ' + '--text' + ' "' + line.split('\t')[0]  + '."')
+
+if opt.session_id != "":
+    
+    try:
+        f = open('input data path', 'r')
+    except:
+        print('no such data or dir')    
+
+    # row[2] : contents / row[3] : talker_identifier / row[4] : title_id / row[1] : line_index
+
+    lines = f.readlines()
+    
+    for line in lines:
+        print(fixed_cmd +' ' + '--speaker_id' + ' ' + 'user input speaker_id' + ' ' + '--text' + ' "' + line  + '."')
+        os.system(fixed_cmd +' ' + '--speaker_id' +' ' + 'user input speaker_id' + ' ' + '--text' + ' "' + line  + '."')
     wav_file_list = subprocess.check_output(['ls ' + temp_dir + '/*.wav'], shell=True, universal_newlines=True)
+    
+    #'/home/workspace/tmp_dir'
+    cnt = 1
+    for wav_file in wav_file_list.split('\n') :
+        if wav_file :
+            print('mv ' + wav_file + ' '  + save_dir  + '/' + cnt + '.wav')
+            os.system('mv ' + wav_file + ' '  + save_dir + '/'  + cnt + '.wav')
+            cnt+=1
+            #'/home/workspace/app/KDT_B1_2/b1project/homepage/static/file_audio/' + opt.id
+            
+            
+            
+else:
+    try:
+        f = open('/home/workspace/folktale_full_data', 'r')
+    except:
+        print('no such data or dir')    
+
+    # row[2] : contents / row[3] : talker_identifier / row[4] : title_id / row[1] : line_index
+
+    lines = f.readlines()
+
+    for line in lines:
+        print(fixed_cmd +' ' + '--speaker_id' + ' ' + line.split('\t')[1] + ' ' + '--text' + ' "' + line.split('\t')[0]  + '."')
+        os.system(fixed_cmd +' ' + '--speaker_id' +' ' + line.split('\t')[1] + ' ' + '--text' + ' "' + line.split('\t')[0]  + '."')
+    wav_file_list = subprocess.check_output(['ls ' + temp_dir + '/*.wav'], shell=True, universal_newlines=True)
+    
+    #'/home/workspace/tmp_dir'
 
     for wav_file in wav_file_list.split('\n') :
         if wav_file :
@@ -50,13 +78,5 @@ for line in lines :
             if not os.path.isdir(save_dir + str(line.split('\t')[2])):
                 os.mkdir(save_dir + str(line.split('\t')[2]))
             os.system('mv ' + wav_file + ' '  + save_dir + str(line.split('\t')[2]) + '/'  + str(line.split('\t')[3].strip()) + '.wav')
-    cnt += 1
-
-#get_files = os.listdir(opt.p)
-
-#num =1
-#for g in get_files:
-#    if g.split('.')[1] == 'wav':  
-#        print('move and rename' + g)
-#        os.rename(opt.p + '/' + g, save_dir +'/' + str(num) +'.wav')
-#        num += 1
+            
+            #'/home/workspace/app/KDT_B1_2/b1project/homepage/static/file_audio/' + opt.id
